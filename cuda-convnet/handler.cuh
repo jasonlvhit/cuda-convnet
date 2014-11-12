ab5fe6b521d930b59bdaa30417be93a81339bb32
Jason Lv <jasonlvhit@gmail.com>
2014-11-12 21:05:11 +0800

    88888888888888

--- cuda-convnet/handler.cuh
+++ cuda-convnet/handler.cuh
@@ -100,7 +100,68 @@ __global__ void compute_err_terms(const float* input, const float* W,
 	}
 }
 
-__global__ void cnnConvolutionalLayerForward(){}
+//void forward(){
+//	for (size_t out = 0; out < out_depth_; out++){
+//		for (size_t in = 0; in < in_depth_; in++){
+//			for (size_t h_ = 0; h_ < out_height_; h_++){
+//				for (size_t w_ = 0; w_ < out_width_; w_++){
+//					output_[getOutIndex(out, h_, w_)] +=
+//						conv(getInforKernel(in, h_, w_), getW_(in, out));
+//				}
+//			}
+//		}
+//		for (size_t h_ = 0; h_ < out_height_; h_++){
+//			for (size_t w_ = 0; w_ < out_width_; w_++){
+//				output_[getOutIndex(out, h_, w_)] =
+//					sigmod(output_[getOutIndex(out, h_, w_)] + /*eh?*/ b_[getb_(out, h_, w_)]);
+//			}
+//		}
+//	}
+//}
+
+void __global__ ConvLayerSigmod(float* output, const float* b){
+	int t_id = threadIdx.x;
+	output[t_id] = 1.0 / (1.0 + expf(output[t_id] + b[t_id]));
+}
+
+void cnnConvolutionalLayerForward(const float* input, const float* W, 
+	const float* b, float* output, size_t in_depth, size_t out_depth){
+	for (size_t i = 0; i < in_depth; i++){
+		for (size_t o = 0; o < out_depth; o++){
+			dim3 grid();
+			dim3 block();
+			convolution << <grid, block >> >();
+		}
+	}
+	cudaDeviceSynchronize();
+	ConvLayerSigmod << < >> >();
+}
+
+__constant__ int kernel_width, in_width, in_heigh, out_width;
+
+__global__ void convolution(const float* input, const float* W, float * output){
+	int out_id = blockIdx.x + out_width * blockIdx.y;
+	int cache_id = threadIdx.x + threadIdx.y * kernel_width;
+	__shared__ float cache[32];
+	if (out_id < out_area){
+		cache[cache_id] = W[cache_id] *
+			input[(blockIdx.y + threadIdx.y) * in_width + blockIdx.x + threadIdx.x];
+	}
+	for (int i = 25; i < 32; i++) cache[i] = 0;
+	__syncthreads();
+
+	int i = 16;
+	cache_id = threadIdx.x + threadIdx.y * blockDim.x;
+	while (i != 0) {
+		if (cache_id < i)
+			cache[cache_id] += cache[cache_id + i];
+		__syncthreads();
+		i /= 2;
+	}
+	if (cache_id == 0 && out_id < out_area) {
+		output[out_id] += cache[0];
+	}
+}
 
 __global__ void cnnConvolutionalLayerBackProp(){}
 
--- cuda-convnet/kernel.cu
+++ cuda-convnet/kernel.cu
@@ -4,39 +4,33 @@
 #include "math_functions.h"
 
 #include "data_descriptor.cuh"
-
+#define KERNEL_WIDTH 5
 #define threadsPerBlock 1024
 __constant__ int out_area, in_area;
 
-__global__ void cnnFullyConnectedLayerForward(const float *input,
-	const float *W, const float *b, float *output) {
-	__shared__ float cache[threadsPerBlock];
-	int tid = threadIdx.x + blockIdx.x * blockDim.x;
-	int cacheIndex = threadIdx.x;
+__constant__ int kernel_width, in_width, in_height, out_width;
 
-	if (cacheIndex < in_area){
-		cache[cacheIndex] = input[cacheIndex] * W[blockIdx.x * in_area + cacheIndex];
+__global__ void convolution(const float* input, const float* W, float * output){
+	int out_id = blockIdx.x + out_width * blockIdx.y;
+	int cache_id = threadIdx.x + threadIdx.y * kernel_width;
+	__shared__ float cache[32];
+	if (out_id < out_area){
+		cache[cache_id] = W[cache_id] *
+			input[(blockIdx.y + threadIdx.y) * in_width + blockIdx.x + threadIdx.x];
 	}
-	else{
-		cache[cacheIndex] = 0;
-	}
-
-	// synchronize threads in this block
+	for (int i = 25; i < 32; i++) cache[i] = 0;
 	__syncthreads();
 
-	// for reductions, threadsPerBlock must be a power of 2
-	// because of the following code
-	int i = blockDim.x / 2;
+	int i =  16;
+	cache_id = threadIdx.x + threadIdx.y * blockDim.x;
 	while (i != 0) {
-		if (cacheIndex < i)
-			cache[cacheIndex] += cache[cacheIndex + i];
+		if (cache_id < i)
+			cache[cache_id] += cache[cache_id + i];
 		__syncthreads();
 		i /= 2;
 	}
-
-	if (cacheIndex == 0) {
-		// sigmod
-		output[blockIdx.x] = 1.0 / (1.0 + expf(cache[0] + b[blockIdx.x]));
+	if (cache_id == 0 && out_id < out_area) {	
+		output[out_id] += cache[0];
 	}
 }
 
@@ -68,7 +62,7 @@ int main()
 	cudaDeviceSynchronize();
 	cudaMemcpy(h_z, d_z, sizeof(float) * h_N, cudaMemcpyDeviceToHost);
 	printf("%f\n", *h_z);
-	*/
+	
 	float *h_input, *h_output, *h_W, *h_b;
 	float *d_input, *d_output, *d_W, *d_b;
 	int h_in_area = 1000;
@@ -115,7 +109,57 @@ int main()
 	for (int i = 0; i < h_out_area; i++){
 		printf("%f\t", h_output[i]);
 	}
+	*/
 
+	float* h_input, *h_output, *h_W;
+	int h_in_width = 16;
+	int h_in_height = 16;
+	int h_in_area = h_in_height * h_in_width;
+	int h_kernel_width = 5;
+	int h_kernel_height = 5;
+	int h_out_width = h_in_width - h_kernel_width + 1;
+	int h_out_height = h_in_height - h_kernel_height + 1;
+	int h_out_area = h_out_width * h_out_height;
+	h_input = (float*)malloc(sizeof(float)*h_in_area);
+	h_output = (float*)malloc(sizeof(float)*h_out_area);
+	h_W = (float*)malloc(sizeof(float)*h_kernel_width * h_kernel_height);
+
+	for (int i = 0; i < h_in_area; i++){
+		h_input[i] = 1.0;
+	}
+
+	for (int i = 0; i < h_out_area; i++){
+		h_output[i] = 0.0;
+	}
+
+	for (int i = 0; i < h_kernel_height * h_kernel_width; i++){
+		h_W[i] = 1.0;
+	}
+
+	float* d_input, *d_output, *d_W;
+	cudaMalloc((void**)&d_input, sizeof(float)*h_in_area);
+	cudaMalloc((void**)&d_output, sizeof(float)*h_out_area);
+	cudaMalloc((void**)&d_W, sizeof(float)*h_kernel_width * h_kernel_height);
+	cudaMemcpy(d_input, h_input, sizeof(float)*h_in_area, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_W, h_W, sizeof(float)*h_kernel_width*h_kernel_width, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_output, h_output, sizeof(float)*h_out_area, cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol(in_area, &h_in_area, sizeof(h_in_area));
+	cudaMemcpyToSymbol(out_area, &h_out_area, sizeof(h_out_area));
+	cudaMemcpyToSymbol(in_width, &h_in_width, sizeof(h_in_width));
+	cudaMemcpyToSymbol(out_width, &h_out_width, sizeof(h_out_width));
+	cudaMemcpyToSymbol(kernel_width, &h_kernel_width, sizeof(h_kernel_height));
+	cudaMemcpyToSymbol(in_height, &h_in_height, sizeof(h_in_height));
+	dim3 grid(h_out_width, h_out_height);
+	dim3 block(8, 8);
+	convolution<<< grid, block >>>(d_input, d_W, d_output);
+	cudaDeviceSynchronize();
+	cudaMemcpy(h_output, d_output, sizeof(float)*h_out_area, cudaMemcpyDeviceToHost);
+	
+	for (int i = 0; i < h_out_area; i++){
+		printf("%f\t", h_output[i]);
+	}
+	
+	
 	getchar();
     return 0;
 }
